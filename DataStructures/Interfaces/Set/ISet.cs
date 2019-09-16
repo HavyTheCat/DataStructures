0b34cfbbf6f8cf908d7e52f3050d9610f1825327
igor <bujhme@gmail.com>
2019-09-16 22:38:03 +0300

    add set implementation

---
+++ DataStructures/Interfaces/Set/ISet.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Interfaces.Set
+{
+    public interface ISet<T> : IEnumerable<T>
+        where T: IComparable<T>
+    {
+        /// <summary>
+        /// Collection of items
+        /// </summary>
+        IEnumerable<T> Items { get; }
+
+        /// <summary>
+        /// Set lenght
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Add data to set
+        /// </summary>
+        /// <param name="data"></param>
+        void Add(T data);
+
+        /// <summary>
+        /// Add rage of data to set
+        /// </summary>
+        /// <param name="dataList"></param>
+        void AddRage(IEnumerable<T> dataList);
+
+        /// <summary>
+        /// Remove specific data from set
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        bool Remove(T data);
+
+        /// <summary>
+        /// Check if set contain specific data 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        bool Contains(T data);
+
+        /// <summary>
+        /// Combine curr untion with another
+        /// </summary>
+        /// <param name="otherSet"></param>
+        /// <returns></returns>
+        ISet<T> Union(ISet<T> otherSet);
+
+        /// <summary>
+        /// REturn intersection of curr set and another
+        /// </summary>
+        /// <param name="otherSet"></param>
+        /// <returns></returns>
+        ISet<T> Intersection(ISet<T> otherSet);
+
+        /// <summary>
+        /// Return only difference of twjo sets
+        /// </summary>
+        /// <param name="otherSet"></param>
+        /// <returns></returns>
+        ISet<T> Difference(ISet<T> otherSet);
+
+        /// <summary>
+        ///  disjunctive union of two sets
+        /// </summary>
+        /// <param name="otherSet"></param>
+        /// <returns></returns>
+        ISet<T> SymmetricDifference(ISet<T> otherSet);
+
+
+
+        
+
+
+    }
+}
---
+++ DataStructures/Set/Set.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures.Interfaces.List;
+using DataStructures.Interfaces.Set;
+
+namespace DataStructures.Set
+{
+    public class Set<T> : Interfaces.Set.ISet<T>
+        where T : IComparable<T>
+    {
+        private readonly IlinkedList<T> _list = new List.LinkedList<T>();
+
+        public int Count => _list.Count;
+
+        public IEnumerable<T> Items { get => _list; }
+
+        public Set()
+        {
+
+        }
+
+        public Set(IEnumerable<T> data)
+        {
+            AddRage(data);
+        }
+
+        public void Add(T data)
+        {
+            if (Contains(data))
+                throw new InvalidOperationException("already in set");
+
+            _list.Add(data);
+        }
+
+        public void AddRage(IEnumerable<T> dataList)
+        {
+            foreach (var data in dataList)
+                Add(data);
+        }
+
+        public bool Contains(T data)
+        {
+            return _list.Contains(data);
+        }
+
+        /// <summary>
+        /// [1, 2, 3, 4] & [2, 3, 4, 5] => [1]
+        /// </summary>
+        /// <param name="otherSet"></param>
+        /// <returns></returns>
+        public Interfaces.Set.ISet<T> Difference(Interfaces.Set.ISet<T> otherSet)
+        {
+            Interfaces.Set.ISet<T> res = new Set<T>(Items);
+            foreach (var item in otherSet.Items)
+                res.Remove(item);
+
+            return res;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        /// <summary>
+        /// [1, 2, 3] & [2, 3, 4, 5] => [2, 3]
+        /// </summary>
+        /// <param name="otherSet"></param>
+        /// <returns></returns>
+        public Interfaces.Set.ISet<T> Intersection(Interfaces.Set.ISet<T> otherSet)
+        {
+            Interfaces.Set.ISet<T> res = new Set<T>();
+            foreach (var data in _list)
+                if (otherSet.Contains(data))
+                    res.Add(data);
+            return res;
+        }
+
+        public bool Remove(T data)
+        {
+            return _list.Remove(data);
+        }
+
+        /// <summary>
+        /// [1, 2, 3] & [2, 3, 4, 5] => [1, 4, 5]
+        /// </summary>
+        /// <param name="otherSet"></param>
+        /// <returns></returns>
+        public Interfaces.Set.ISet<T> SymmetricDifference(Interfaces.Set.ISet<T> otherSet)
+        {
+            var itersecSet = Intersection(otherSet);
+            var unionSet = Union(otherSet);
+
+            return unionSet.Difference(itersecSet);
+        }
+
+        /// <summary>
+        /// [1, 2, 3] & [2, 3, 4, 5] => [1, 2, 3, 4, 5]
+        /// </summary>
+        /// <param name="otherSet"></param>
+        /// <returns></returns>
+        public Interfaces.Set.ISet<T> Union(Interfaces.Set.ISet<T> otherSet)
+        {
+            var res = new Set<T>(_list);
+            res.AddRageSkipDuplicates(otherSet.Items);
+            return res;
+        }
+
+        private void AddRageSkipDuplicates(IEnumerable<T> items)
+        {
+            foreach (var item in items)
+                if (!_list.Contains(item))
+                    Add(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
